421e20c2b633943bddf265399363ff21fd9f2019
renyuxiang <renyuxiang@gengmei.com>
2018-11-27 10:30:54 +0800

    Initial commit

---
+++ app/src/main/java/com/shark/anchorscrollview/demo/MainActivity.java
@@ -0,0 +1,231 @@
+package com.shark.anchorscrollview.demo;
+
+import android.content.Context;
+import android.databinding.DataBindingUtil;
+import android.graphics.Color;
+import android.os.Build;
+import android.support.annotation.NonNull;
+import android.support.v4.widget.NestedScrollView;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Button;
+import android.widget.SimpleAdapter;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.shark.anchorscrollview.AnchorScrollView;
+import com.shark.anchorscrollview.demo.databinding.ActivityMainBinding;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding binding;
+    private TestAdapter testAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        testAdapter = new TestAdapter(getData());
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        binding.webView1.getSettings().setJavaScriptEnabled(true);
+        binding.webView1.setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                view.loadUrl(url);
+                return true;
+            }
+        });
+        binding.webView2.getSettings().setJavaScriptEnabled(true);
+        binding.webView2.setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                view.loadUrl(url);
+                return true;
+            }
+        });
+        binding.webView3.setWebViewClient(new WebViewClient() {
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                super.onPageFinished(view, url);
+                view.setVisibility(View.GONE);
+            }
+
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                view.loadUrl(url);
+                return true;
+            }
+        });
+
+        binding.webView1.loadUrl("http://www.waijiaoyi.com");
+        binding.webView2.loadUrl("http://www.sina.com");
+        binding.webView3.loadUrl("https://baike.baidu.com/item/大同月饼");
+
+        binding.recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        binding.recyclerView.setAdapter(testAdapter);
+
+        binding.button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setButtonState(binding.textView);
+                binding.mainScroll.anchorView(binding.textView);
+            }
+        });
+        binding.button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setButtonState(binding.textView2);
+                binding.mainScroll.anchorView(binding.textView2);
+            }
+        });
+        binding.button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setButtonState(binding.webView1);
+                binding.mainScroll.anchorView(binding.webView1);
+            }
+        });
+        binding.button4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setButtonState(binding.webView2);
+                binding.mainScroll.anchorView(binding.webView2);
+            }
+        });
+        binding.button6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setButtonState(binding.wikiLayout);
+                binding.mainScroll.anchorView(binding.wikiLayout);
+            }
+        });
+        binding.button7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setButtonState(binding.recyclerView);
+                binding.mainScroll.anchorView(binding.recyclerView);
+            }
+        });
+        binding.button5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                binding.webView3.setVisibility(binding.webView3.getVisibility() == View.VISIBLE ? View.GONE : View.VISIBLE);
+                if (binding.webView3.getVisibility() == View.VISIBLE)
+                    binding.mainScroll.anchorView(binding.wikiLayout);
+            }
+        });
+        binding.mainScroll.addAnchorView(binding.textView, binding.textView2, binding.webView1, binding.webView2, binding.wikiLayout, binding.recyclerView);
+        binding.mainScroll.setOnNestedScrollViewChangedListener(new AnchorScrollView.OnNestedScrollViewChangedListener() {
+            @Override
+            public void onAnchor(View view) {
+                if (view == null) return;
+                setButtonState(view);
+            }
+
+            @Override
+            public void onScrollStop() {
+
+            }
+
+            @Override
+            public void onScrollToBottom() {
+                testAdapter.getDataList().addAll(getData());
+                testAdapter.notifyDataSetChanged();
+                Toast.makeText(MainActivity.this, "滑到底部加载更多", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    public List<String> getData() {
+        List<String> list = new ArrayList<>();
+        for (int i = 0; i < 20; i++) {
+            list.add("<" + i + ">");
+        }
+        return list;
+    }
+
+    public void setButtonState(View view) {
+        binding.button.setTextColor(Color.BLACK);
+        binding.button2.setTextColor(Color.BLACK);
+        binding.button3.setTextColor(Color.BLACK);
+        binding.button4.setTextColor(Color.BLACK);
+        binding.button6.setTextColor(Color.BLACK);
+        binding.button7.setTextColor(Color.BLACK);
+
+        if (view.getId() == binding.textView.getId()) {
+            binding.button.setTextColor(Color.YELLOW);
+        }
+        if (view.getId() == binding.textView2.getId()) {
+            binding.button2.setTextColor(Color.YELLOW);
+        }
+        if (view.getId() == binding.webView1.getId()) {
+            binding.button3.setTextColor(Color.YELLOW);
+        }
+        if (view.getId() == binding.webView2.getId()) {
+            binding.button4.setTextColor(Color.YELLOW);
+        }
+        if (view.getId() == binding.wikiLayout.getId()) {
+            binding.button6.setTextColor(Color.YELLOW);
+        }
+        if (view.getId() == binding.recyclerView.getId()) {
+            binding.button7.setTextColor(Color.YELLOW);
+        }
+    }
+
+    private static class TestAdapter extends RecyclerView.Adapter {
+        private List<String> dataList;
+
+        public TestAdapter(List<String> dataList) {
+            this.dataList = dataList;
+        }
+
+        public static class ViewHolder extends RecyclerView.ViewHolder {
+
+            public ViewHolder(View v) {
+                super(v);
+            }
+
+        }
+
+        @NonNull
+        @Override
+        public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+            TextView textView = new TextView(viewGroup.getContext());
+            textView.setTextColor(Color.BLACK);
+            textView.setLayoutParams(new RecyclerView.LayoutParams(-1, -2));
+            return new ViewHolder(textView);
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull RecyclerView.ViewHolder viewHolder, int i) {
+            TextView textView = (TextView) viewHolder.itemView;
+            textView.setText("Item:" + i);
+            textView.setGravity(Gravity.CENTER);
+        }
+
+        @Override
+        public int getItemCount() {
+            return dataList.size();
+        }
+
+        public List<String> getDataList() {
+            return dataList;
+        }
+
+        public void setDataList(List<String> dataList) {
+            this.dataList = dataList;
+        }
+    }
+
+}
---
+++ library/src/main/java/com/shark/anchorscrollview/AnchorScrollView.java
@@ -0,0 +1,119 @@
+package com.shark.anchorscrollview;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.widget.NestedScrollView;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.View;
+
+import com.shark.utils.library.core.SLog;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class AnchorScrollView extends NestedScrollView {
+    private Set<View> anchorViewSet;
+    private OnNestedScrollViewChangedListener onNestedScrollViewChangedListener;
+    private OnScrollChangeListener customerScrollChangeListener;
+    private OnScrollChangeListener onScrollChangeListener = new OnScrollChangeListener() {
+        @Override
+        public void onScrollChange(NestedScrollView nestedScrollView, int scrollX, int scrollY, int oldScrollX, int oldScrollY) {
+            removeCallbacks(scrollStateChangeTask);
+            postDelayed(scrollStateChangeTask, 100);
+
+            if (customerScrollChangeListener == null) return;
+            customerScrollChangeListener.onScrollChange(nestedScrollView, scrollX, scrollY, oldScrollX, oldScrollY);
+//            if (Math.abs(scrollY-oldScrollY)<=2||scrollY==0||(scrollY+getMeasuredHeight())==getChildAt(0).getMeasuredHeight()) SLog.e(this, "Scroll stop");
+
+        }
+    };
+    private Runnable scrollStateChangeTask = new Runnable() {
+        @Override
+        public void run() {
+            if (onNestedScrollViewChangedListener == null) return;
+            onNestedScrollViewChangedListener.onScrollStop();
+            onNestedScrollViewChangedListener.onAnchor(findAnchor(getScrollY()));
+
+            if ((getScrollY() + getMeasuredHeight()) < getChildAt(0).getMeasuredHeight()) return;
+            onNestedScrollViewChangedListener.onScrollToBottom();
+        }
+    };
+
+    public AnchorScrollView(@NonNull Context context) {
+        super(context);
+        init();
+
+    }
+
+    public AnchorScrollView(@NonNull Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+        init();
+    }
+
+    public AnchorScrollView(@NonNull Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init();
+    }
+
+    @Override
+    protected void onLayout(boolean changed, int l, int t, int r, int b) {
+        super.onLayout(changed, l, t, r, b);
+    }
+
+    private void init() {
+        super.setOnScrollChangeListener(onScrollChangeListener);
+        anchorViewSet = new HashSet<>();
+    }
+
+    public void anchorView(final View view) {
+        post(new Runnable() {
+            @Override
+            public void run() {
+                scrollTo(0, view.getTop());
+            }
+        });
+    }
+
+    public void addAnchorView(View... view) {
+        if (view == null) return;
+        for (View v : view) {
+            anchorViewSet.add(v);
+        }
+    }
+
+    public void removeAnchorView(View view) {
+        anchorViewSet.remove(view);
+    }
+
+    private View findAnchor(int scrollY) {
+        for (View view : anchorViewSet) {
+            if (scrollY < view.getTop()) continue;
+            if (scrollY >= view.getBottom()) continue;
+            return view;
+        }
+        return null;
+    }
+
+    @Override
+    public void setOnScrollChangeListener(OnScrollChangeListener listener) {
+        customerScrollChangeListener = listener;
+    }
+
+    public OnNestedScrollViewChangedListener getOnNestedScrollViewChangedListener() {
+        return onNestedScrollViewChangedListener;
+    }
+
+    public void setOnNestedScrollViewChangedListener(OnNestedScrollViewChangedListener onNestedScrollViewChangedListener) {
+        this.onNestedScrollViewChangedListener = onNestedScrollViewChangedListener;
+    }
+
+    public interface OnNestedScrollViewChangedListener {
+        void onAnchor(View view);
+
+        void onScrollStop();
+
+        void onScrollToBottom();
+    }
+}
